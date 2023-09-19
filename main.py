4c20f79912ae8a1b7388506bed24d9420f6ac6cc
Amine <amine.dhane@epfedu.fr>
2023-09-19 14:54:49 +0200

    Fifth Commit

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
 def hello_world():
     prefix_google = """
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z74QSEWTX5">
     </script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'UA-285804915-2');
+        gtag('config', 'G-Z74QSEWTX5');
     </script>
         """
     return prefix_google + "Hello World"
-
-
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-@app.route("/mechantmechant")
+@app.route("/moon")
 def mechant():
     return "Hello from Space! 🚀"
